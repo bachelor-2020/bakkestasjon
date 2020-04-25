8f96dbc9b43c037026180dfe9e65ed25177d846f
Lars Rustand <rustand.lars@gmail.com>
2020-04-25 13:43:40 +0200

    Utkast til api endpoints

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__, static_url_path='')
 
@@ -6,4 +6,39 @@ app = Flask(__name__, static_url_path='')
 def index():
     return app.send_static_file("index.html")
 
-app.run()
+
+# Hent nåværende mission for drone
+@app.route("/api/drones/<drone_id>/mission")
+def get_drone_mission(drone_id):
+    return jsonify(
+        drone_id = drone_id,
+        mission = [],
+        searched = []
+    )
+
+# Hent posisjon for drone
+@app.route("/api/drones/<drone_id>/position")
+def get_drone_pos(drone_id):
+    return jsonify(
+        drone_id = drone_id,
+        pos = [10,50]
+    )
+
+# Last opp ny mission til drone
+@app.route("/api/drones/<drone_id>/mission", methods=["POST"])
+def post_drone_mission(drone_id):
+    return request.json
+
+# Last opp ny mission
+@app.route("/api/mission", methods=["POST"])
+def post_mission():
+    return request.json
+
+# Hent mission
+@app.route("/api/mission")
+def get_mission():
+    return jsonify(
+        mission = []
+    )
+
+app.run(host="0.0.0.0")
